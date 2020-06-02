d543bb86a1780207082149d49cc35d2ef96630af
vmanav <zmanav.1999@gmail.com>
2020-06-02 19:57:30 +0530

    configureStore() setup, and add initial todosSlice

---
+++ src/features/todos/todosSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const todosSlice = createSlice({
+  // Prefix for generated Action types
+  name: 'todos',
+  // the initial state value for the reducer
+  initialState: [],
+  // an object, where the keys will become action type strings,
+  // and the functions are reducers that will be run when that action type is dispatched. 
+  reducers: {
+    // Action Type: "todos/addTodo"
+    addTodo(state, action) {
+      const { id, text } = action.payload;
+      state.push({ id, text, completed: false });
+    },
+    // Action Type: "todos/toggleTodo"
+    toggleTodo(state, action) {
+      const todo = state.find(todo => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    }
+    // NO DEFAULT HANDLER REQD
+  }
+})
+// NOTE: We can do "mutation of STATE", bcoz BTS `produce` from `immer libarary` is used
+
+// todosSlice => 
+/*
+{
+  name: "todos",
+  reducer: (state, action) => newState,
+  actions: {
+    addTodo: (payload) => ({type: "todos/addTodo", payload}),
+    toggleTodo: (payload) => ({type: "todos/toggleTodo", payload})
+  },
+  caseReducers: {
+    addTodo: (state, action) => newState,
+    toggleTodo: (state, action) => newState,
+  }
+}
+*/
+
+// ActionCreators
+export const { addTodo, toggleTodo } = todosSlice.actions;
+
+// Reducer Function
+export default todosSlice.reducer;
