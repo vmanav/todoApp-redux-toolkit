788d108231d524ef01fa69f78472d7299d0d291d
vmanav <zmanav.1999@gmail.com>
2020-06-02 23:20:45 +0530

    use the filterSlicereducer, and use new filters in actions UI

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,9 +14,3 @@ export const toggleTodo = id => ({
   type: 'TOGGLE_TODO',
   id
 })
-
-export const VisibilityFilters = {
-  SHOW_ALL: 'SHOW_ALL',
-  SHOW_COMPLETED: 'SHOW_COMPLETED',
-  SHOW_ACTIVE: 'SHOW_ACTIVE'
-}
